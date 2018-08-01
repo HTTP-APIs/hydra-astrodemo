4ca01d06cfd32e9dbf413f4fcbbdcab1288f8366
sandeepsajan0 <sandeepsajan0@gmail.com>
2018-08-01 15:01:58 +0530

    querying process working fine

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template,request,session
 from hydra_redis import querying_mechanism
-from hydra_redis import querying_mechanism
+from hydrus.hydraspec import doc_maker
 
 app = Flask(__name__)
 app.secret_key = "any random string"
@@ -16,35 +16,37 @@ def enter_url():
     if request.method == 'POST':
         if 'url' in request.form:
             url= request.form['url']
-            print(request.form)
             session['url'] = url
             handle_data = querying_mechanism.HandleData()
-            global apidoc
-            apidoc = handle_data.load_data(url)
-            print(url,apidoc)
-            session['entrypoint'] = apidoc
-            return render_template("index.html", apidoc = apidoc)
-        elif 'query' in request.form:
+            global api_doc
+            endpoints = handle_data.load_data(url)
+            session['entrypoint'] = endpoints
+            apidoc1 = handle_data.load_data(url + "/vocab")
+            api_doc = doc_maker.create_doc(apidoc1)
+            global facades
+            facades = querying_mechanism.QueryFacades(api_doc, session['url'],False)
+            facades.initialize(True)
+            print(url,endpoints)
+            return render_template("index.html", apidoc = endpoints)
+#        elif 'query' in request.form:
+#            query = request.form['query']
+#            print(query)
+#            print(session['url'])
+#            return render_template("index.html",apidoc)
+
+
+@app.route('/query',methods=['GET','POST'])
+def enter_query():
+    if request.method == 'GET':
+        return render_template("index.html")
+    if request.method == 'POST':
+        if 'query' in request.form:
+            print("here")
+            print(session['url'])
             query = request.form['query']
             print(query)
-            print(session['url'])
-            return render_template("index.html",apidoc)
-@app.route('/query',methods=['POST'])
-def enter_query():
-    print("here")
-    print(request.form)
-    print(session['url'])
-    print(session['entrypoint'])
-    return "Happy"
-
-#@app.route('/', methods= ['GET','POST'])
-#def enter_query():
-#    if request.method == 'GET':
-#        return render_template("index.html")
-#    if request.method == 'POST':
-#        query = request.form['query']
-#        print(query)
-#        return render_template("index.html")
+            output = facades.user_query(query)
+        return render_template("index.html", query_output = output)
 
 if __name__ == "__main__":
     app.run()
