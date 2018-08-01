19858fc2b9ee15cd9212912ff4f93a72e81b8805
vaibhavchellani <vaibhavchellani223@gmail.com>
2018-08-01 11:47:38 +0530

    added session

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,session
 from hydra_redis import querying_mechanism
 from hydra_redis import querying_mechanism
 
 app = Flask(__name__)
+app.secret_key = "any random string"
 
 @app.route('/')
 def index():
@@ -15,6 +16,7 @@ def enter_url():
     if request.method == 'POST':
         if 'url' in request.form:
             url= request.form['url']
+            session['url'] = url
             handle_data = querying_mechanism.HandleData()
             global apidoc
             apidoc = handle_data.load_data(url)
@@ -23,7 +25,13 @@ def enter_url():
         elif 'query' in request.form:
             query = request.form['query']
             print(query)
+            print(session['url'])
             return render_template("index.html",apidoc)
+@app.route('/query',methods=['POST'])
+def enter_query():
+    print("here")
+    print(session['url'])
+    return "Happy"
 
 #@app.route('/', methods= ['GET','POST'])
 #def enter_query():
